ce560d249fbca602d20ceebf843892811c192da0
white333333 <107554036+white333333@users.noreply.github.com>
2026-04-23 20:26:12 +0200

    add token counting functionality

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
     )
+    #counting tokens:
+    if response.usage_metadata is None:
+        raise RuntimeError("failed API request")
+    else:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
     print(response.text)
 
 if __name__ == "__main__":
