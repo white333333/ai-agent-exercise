491e374a50d4ff0a72be60b3689731ea94621bba
white333333 <107554036+white333333@users.noreply.github.com>
2026-04-24 18:55:01 +0200

    add user input functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,8 +10,12 @@ def main():
         raise RuntimeError("api key not found")
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.5-flash', contents= args.user_prompt
     )
     #counting tokens:
     if response.usage_metadata is None:
