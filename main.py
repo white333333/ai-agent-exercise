7db6ca32114a473e600e1c7375da79ff17fad209
white333333 <107554036+white333333@users.noreply.github.com>
2026-04-25 11:08:15 +0200

    add multiple message storing

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
-        raise RuntimeError("api key not found")
+        raise RuntimeError("API key not found")
     client = genai.Client(api_key=api_key)
 
+    
+
+    #parsing the input
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
+    #response
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents= args.user_prompt
+        model='gemini-2.5-flash', contents=messages
     )
     #counting tokens:
     if response.usage_metadata is None:
