ab6b35c014ebe770c26814110ffbb356fd37d4fa
white333333 <107554036+white333333@users.noreply.github.com>
2026-04-27 22:49:59 +0200

    add --verbose output fucntionality

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
 
     #parsing the input
     parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -24,11 +25,13 @@ def main():
         model='gemini-2.5-flash', contents=messages
     )
     #counting tokens:
-    if response.usage_metadata is None:
-        raise RuntimeError("failed API request")
-    else:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        if response.usage_metadata is None:
+            raise RuntimeError("failed API request")
+        else:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
     print(response.text)
